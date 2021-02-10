ae7193a8d92138b49b1f7e3539bf17b8ccf81cf1
Gannon Higgins <48813409+GannonHiggins@users.noreply.github.com>
2021-02-09 18:50:16 -0900

    Update test_code.js

--- test_code.js
+++ test_code.js
@@ -1,93 +1,77 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const userGrid = document.querySelector('.grid-user')
-    const computerGrid = document.querySelector('.grid-computer')
-    const displayGrid = document.querySelector('.grid-display')
-    const ships = document.querySelectorAll('.ship')
-    const destroyer = document.querySelector('.destroyer-container')
-    const submarine = document.querySelector('.submarine-container')
-    const cruiser = document.querySelector('.cruiser-container')
-    const battleship = document.querySelector('.battleship-container')
-    const carrier = document.querySelector('.carrier-container')
-    const startButton = document.querySelector('#start')
-    const rotateButton = document.querySelector('#rotate')
-    const turnDisplay = document.querySelector('#whose-go')
-    const infoDisplay = document.querySelector('#info')
-    const userSquares = []
-    const computerSquares = []
-    let isHorizontal = true;
-    let isGameOver = false
-    let currentPlayer = 'user'
+  const userGrid = document.querySelector('.grid-user')
+  const computerGrid = document.querySelector('.grid-computer')
+  const displayGrid = document.querySelector('.grid-display')
+  const ships = document.querySelectorAll('.ship')
+  const destroyer = document.querySelector('.destroyer-container')
+  const submarine = document.querySelector('.submarine-container')
+  const cruiser = document.querySelector('.cruiser-container')
+  const battleship = document.querySelector('.battleship-container')
+  const carrier = document.querySelector('.carrier-container')
+  const startButton = document.querySelector('#start')
+  const rotateButton = document.querySelector('#rotate')
+  const turnDisplay = document.querySelector('#whose-go')
+  const infoDisplay = document.querySelector('#info')
+  const userSquares = []
+  const computerSquares = []
+  let isHorizontal = true
+  let isGameOver = false
+  let currentPlayer = 'user'
+  const width = 10
 
-
-    const width = 10
-
-    //create boards
-    function createBoard(grid, squares){
-        for(let i=0; i< width*width; i++){
-            const square = document.createElement('div')
-            square.dataset.id = i
-            grid.appendChild(square)
-            squares.push(square) 
-
-        }
-
+  //Create Board
+  function createBoard(grid, squares) {
+    for (let i = 0; i < width*width; i++) {
+      const square = document.createElement('div')
+      square.dataset.id = i
+      grid.appendChild(square)
+      squares.push(square)
     }
+  }
+  createBoard(userGrid, userSquares)
+  createBoard(computerGrid, computerSquares)
 
-    createBoard(userGrid, userSquares)
-    createBoard(computerGrid, computerSquares)
+  //Ships
+  const shipArray = [
+    {
+      name: 'destroyer',
+      directions: [
+        [0, 1],
+        [0, width]
+      ]
+    },
+    {
+      name: 'submarine',
+      directions: [
+        [0, 1, 2],
+        [0, width, width*2]
+      ]
+    },
+    {
+      name: 'cruiser',
+      directions: [
+        [0, 1, 2],
+        [0, width, width*2]
+      ]
+    },
+    {
+      name: 'battleship',
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, width*2, width*3]
+      ]
+    },
+    {
+      name: 'carrier',
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, width*2, width*3, width*4]
+      ]
+    },
+  ]
 
-    //ships
-    const shipArray = [
-        {
-        name: 'destroyer', 
-        directions: [
-            [0,1],
-            [0,width]
-       ]
-        },
-
-        {
-            name: 'submarine',
-            directions: [
-
-                [0,1,2],
-                [0,width,width*2]
-
-            ]
-        },
-        {
-            name: 'cruiser',
-            directions: [
-
-                [0,1,2],
-                [0,width,width*2]
-
-            ]
-        },
-        {
-            name: 'battleship',
-            directions: [
-
-                [0,1,2,3],
-                [0,width,width*2,width*3]
-
-            ]
-        },
-        {
-            name: 'carrier',
-            directions: [
-
-                [0,1,2,3,4],
-                [0,width,width*2,width*3, width*4]
-
-            ]
-        },
-    ]
-
-
-
- //Draw the computers ships in random locations
- function generate(ship) {
+  //Draw the computers ships in random locations
+  function generate(ship) {
     let randomDirection = Math.floor(Math.random() * ship.directions.length)
     let current = ship.directions[randomDirection]
     if (randomDirection === 0) direction = 1
@@ -102,15 +86,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     else generate(ship)
   }
+  generate(shipArray[0])
+  generate(shipArray[1])
+  generate(shipArray[2])
+  generate(shipArray[3])
+  generate(shipArray[4])
 
-generate(shipArray[0])
-generate(shipArray[1])
-generate(shipArray[2])
-generate(shipArray[3])
-generate(shipArray[4])
-
-//rotate the ships
-function rotate() {
+  //Rotate the ships
+  function rotate() {
     if (isHorizontal) {
       destroyer.classList.toggle('destroyer-container-vertical')
       submarine.classList.toggle('submarine-container-vertical')
@@ -132,46 +115,45 @@ function rotate() {
       return
     }
   }
+  rotateButton.addEventListener('click', rotate)
 
-rotateButton.addEventListener('click', rotate)
+  //move around user ship
+  ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
+  userSquares.forEach(square => square.addEventListener('dragstart', dragStart))
+  userSquares.forEach(square => square.addEventListener('dragover', dragOver))
+  userSquares.forEach(square => square.addEventListener('dragenter', dragEnter))
+  userSquares.forEach(square => square.addEventListener('dragleave', dragLeave))
+  userSquares.forEach(square => square.addEventListener('drop', dragDrop))
+  userSquares.forEach(square => square.addEventListener('dragend', dragEnd))
 
-//move around user ship 
+  let selectedShipNameWithIndex
+  let draggedShip
+  let draggedShipLength
 
-ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
-userSquares.forEach(square => square.addEventListener('dragstart', dragStart))
-userSquares.forEach(square => square.addEventListener('dragover', dragOver))
-userSquares.forEach(square => square.addEventListener('dragenter', dragEnter))
-userSquares.forEach(square => square.addEventListener('dragleave', dragLeave))
-userSquares.forEach(square => square.addEventListener('drop', dragDrop))
-userSquares.forEach(square => square.addEventListener('dragend', dragEnd))
+  ships.forEach(ship => ship.addEventListener('mousedown', (e) => {
+    selectedShipNameWithIndex = e.target.id
+    console.log(selectedShipNameWithIndex)
+  }))
 
-let selectedShipNameWithIndex
-let draggedShip
-let draggedShipLength
-
-ships.forEach(ship => ship.addEventListener('mousedown',(e) => {
-selectedShipNameWithIndex = e.target.id
-}))
-
-function dragStart(){
+  function dragStart() {
     draggedShip = this
     draggedShipLength = this.childNodes.length
     console.log(draggedShip)
-}
+  }
 
-function dragOver(e){
-    e.preventDefault() 
-}
-
-function dragEnter(e){
+  function dragOver(e) {
     e.preventDefault()
-}
+  }
 
-function dragLeave(){
+  function dragEnter(e) {
+    e.preventDefault()
+  }
+
+  function dragLeave() {
     console.log('drag leave')
-}
+  }
 
-function dragDrop(){
+  function dragDrop() {
     let shipNameWithLastId = draggedShip.lastChild.id
     let shipClass = shipNameWithLastId.slice(0, -2)
     console.log(shipClass)
@@ -193,7 +175,7 @@ function dragDrop(){
       for (let i=0; i < draggedShipLength; i++) {
         userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass)
       }
-    //As long as the index of the ship is not in the newNotAllowedVertical. This means that sometimes if you drag the ship by its
+    //As long as the index of the ship you are dragging is not in the newNotAllowedVertical array! This means that sometimes if you drag the ship by its
     //index-1 , index-2 and so on, the ship will rebound back to the displayGrid.
     } else if (!isHorizontal && !newNotAllowedVertical.includes(shipLastId)) {
       for (let i=0; i < draggedShipLength; i++) {
@@ -202,38 +184,36 @@ function dragDrop(){
     } else return
 
     displayGrid.removeChild(draggedShip)
-}
+  }
 
-function dragEnd(){
+  function dragEnd() {
     console.log('dragend')
-}
+  }
 
-//Game logic
-
-function playGame(){
+  //Game Logic
+  function playGame() {
     if (isGameOver) return
-    if (currentPlayer === 'user'){
-        turnDisplay.innerHTML = 'Your Go'
-        computerSquares.forEach(square => square.addEventListener('click', function(e) {
-            revealSquare(square)
-          }))
+    if (currentPlayer === 'user') {
+      turnDisplay.innerHTML = 'Your Go'
+      computerSquares.forEach(square => square.addEventListener('click', function(e) {
+        revealSquare(square)
+      }))
     }
-    if (currentPlayer === 'computer'){
-        turnDisplay.innerHTML = 'computers Go'
-        setTimeout(computerGo, 1000)
-        //function computerGO
+    if (currentPlayer === 'computer') {
+      turnDisplay.innerHTML = 'Computers Go'
+      setTimeout(computerGo, 1000)
     }
-}
+  }
+  startButton.addEventListener('click', playGame)
 
-startButton.addEventListener('click', playGame)
+  let destroyerCount = 0
+  let submarineCount = 0
+  let cruiserCount = 0
+  let battleshipCount = 0
+  let carrierCount = 0
 
-let destroyerCount = 0
-let submarineCount = 0
-let cruiserCount = 0
-let battleshipCount = 0
-let carrierCount = 0
 
-function revealSquare(square) {
+  function revealSquare(square) {
     if (!square.classList.contains('boom')) {
       if (square.classList.contains('destroyer')) destroyerCount++
       if (square.classList.contains('submarine')) submarineCount++
@@ -257,8 +237,8 @@ function revealSquare(square) {
   let cpuBattleshipCount = 0
   let cpuCarrierCount = 0
 
-  
-function computerGo() {
+
+  function computerGo() {
     let random = Math.floor(Math.random() * userSquares.length)
     if (!userSquares[random].classList.contains('boom')) {
       userSquares[random].classList.add('boom')
@@ -328,5 +308,4 @@ function computerGo() {
     isGameOver = true
     startButton.removeEventListener('click', playGame)
   }
-
-}) 
+})
