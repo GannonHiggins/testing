945cc29c8d355e01c73906e5d52e4f037f2ff955
Gannon Higgins <akg98@me.com>
2021-02-15 16:57:28 -0900

    created json file

--- test_code.js
+++ test_code.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+ document.addEventListener('DOMContentLoaded', () => {
     const userGrid = document.querySelector('.grid-user')
     const computerGrid = document.querySelector('.grid-computer')
     const displayGrid = document.querySelector('.grid-display')
