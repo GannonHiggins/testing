23056030466cc48bc72c43bbbd99db137c12763e
Gannon Higgins <akg98@me.com>
2021-02-15 18:40:09 -0900

    made final changes to files for networking

--- server.js
+++ server.js
@@ -7,41 +7,83 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-
-
-//set static folder
+// Set static folder
 app.use(express.static(path.join(__dirname, "public")))
 
-//start server
-server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+// Start server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-//handle a socket connection request from web client 
+// Handle a socket connection request from web client
 const connections = [null, null]
 
-io.on('connection', socket =>{
-    //console.log('New WS Connection')
+io.on('connection', socket => {
+  // console.log('New WS Connection')
 
-    //find an available player number
-    let playerIndex = -1;
-    for(const i in connections){
-        if (connections[i] === null){
-            playerIndex = i
-            break
-        }
+  // Find an available player number
+  let playerIndex = -1;
+  for (const i in connections) {
+    if (connections[i] === null) {
+      playerIndex = i
+      break
     }
+  }
 
-    //tell the connecting client what player number they are 
-    socket.emit('player-number', playerIndex)
+  // Tell the connecting client what player number they are
+  socket.emit('player-number', playerIndex)
 
-    console.log(`Player ${playerIndex} has connected`)
+  console.log(`Player ${playerIndex} has connected`)
 
+  // Ignore player 3
+  if (playerIndex === -1) return
 
-    //ignore player 3
-    if (playerIndex === -1) return
+  connections[playerIndex] = false
 
-    connections[playerIndex] = false
+  // Tell eveyone what player number just connected
+  socket.broadcast.emit('player-connection', playerIndex)
 
-    //tell everyone what player number just connected 
-
+  // Handle Diconnect
+  socket.on('disconnect', () => {
+    console.log(`Player ${playerIndex} disconnected`)
+    connections[playerIndex] = null
+    //Tell everyone what player numbe just disconnected
     socket.broadcast.emit('player-connection', playerIndex)
+  })
+
+  // On Ready
+  socket.on('player-ready', () => {
+    socket.broadcast.emit('enemy-ready', playerIndex)
+    connections[playerIndex] = true
+  })
+
+  // Check player connections
+  socket.on('check-players', () => {
+    const players = []
+    for (const i in connections) {
+      connections[i] === null ? players.push({connected: false, ready: false}) : players.push({connected: true, ready: connections[i]})
+    }
+    socket.emit('check-players', players)
+  })
+
+  // On Fire Received
+  socket.on('fire', id => {
+    console.log(`Shot fired from ${playerIndex}`, id)
+
+    // Emit the move to the other player
+    socket.broadcast.emit('fire', id)
+  })
+
+  // on Fire Reply
+  socket.on('fire-reply', square => {
+    console.log(square)
+
+    // Forward the reply to the other player
+    socket.broadcast.emit('fire-reply', square)
+  })
+
+  // Timeout connection
+  setTimeout(() => {
+    connections[playerIndex] = null
+    socket.emit('timeout')
+    socket.disconnect()
+  }, 600000) // 10 minute limit per player
 })
